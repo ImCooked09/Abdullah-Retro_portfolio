d85738df4c8486dbad60ca90f1c2b096b940bd3d
Abdullah Salman <salmanabdullah619@gmail.com>
2024-10-27 17:38:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+
+
+var tl = gsap.timeline();
+
+tl.to(".page1", {
+  y: "100vh",
+  scale: 0.6,
+  duration: 0,
+});
+
+tl.to(".page1", {
+  y: "30vh",
+  duration: 1,
+  delay: 1,
+});
+tl.to(".page1", {
+  y: "0vh",
+  rotate: 360,
+  scale: 1,
+  duration:0.9
+});
+
+Shery.mouseFollower();
+
+Shery.makeMagnet(".nav a");
+Shery.makeMagnet(".nav img");
